f2d4aaa9e9cd92ec4130fa81467721364c0c57e1
CainWeideman <c.weideman@student.rug.nl>
2024-10-08 20:58:56 +0200

    You can now define page ranges

--- scripts/job_counter.py
+++ scripts/job_counter.py
@@ -3,42 +3,50 @@ from collections import Counter
 
 
 reader = PdfReader("wetransfer_adresboeken-laatste_2024-10-07_1513/1769_19525-1964.pdf") # Read the pdf
-page = reader.pages[184] # Pick a page from the pdf
-text = page.extract_text() # Extract the text from the page
+big_dict = {}
 
-# Added spaces around newline characters and replace some other characters
-text = text.replace("\n", " \n ")
-text = text.replace("..", ".,")
-text = text.replace("\n", "")
-text = text.replace("-  ", "")
-text = text.replace("\xad", "")
+for i in range(100, 598): # define the range of pages
+	page_number = i
+	print("Page: ", page_number)
+	page = reader.pages[i] # Pick a page from the pdf, 184 works
+	text = page.extract_text() # Extract the text from the page
 
-text_list = text.split("   ") # Split the text on three spaces
+	# Added spaces around newline characters and replace some other characters
+	text = text.replace("\n", " \n ")
+	text = text.replace("..", ".,")
+	text = text.replace("\n", "")
+	text = text.replace("-  ", "")
+	text = text.replace("\xad", "")
 
-text_strip = [sentence.strip() for sentence in text_list] # Remove the excess spaces around the sentence and add the sentence to a list
+	text_list = text.split("   ") # Split the text on three spaces
 
-# List that can be used to determine if a phrase is a street etc.
-address_list = ["str", "laan", "weg", "singel", "diep", "w.", "s.", "k.", "kade"]
+	text_strip = [sentence.strip() for sentence in text_list] # Remove the excess spaces around the sentence and add the sentence to a list
 
-job_list = [] # The list of jobs mentioned on the page
+	# List that can be used to determine if a phrase is a street etc.
+	address_list = ["str", "laan", "weg", "singel", "diep", "w.", "s.", "k.", "kade", "ged"]
 
-for sentence in text_strip:
-	splits = sentence.split(",") # Split the sentence on commas
-	if sentence[0][0] == "-": # Determine if the first character of the sentence is "-"
-		job_index = 1 # If the sentence starts with "-", the job is often the second phrase of the sentence
-	else:
-		job_index = 2 # Job is often the third phrase of the sentence, due to full names of the people
-	
-	if len(splits) > job_index: # Prevent index out of range
-		job = splits[job_index] # Extract the job phrase
-		job = job.strip(" ") # Remove the spaces on both sides of the phrase
-		if len(job) > 3: # Sometimes "van" or "der" or at the index of the ob, so we exclude those
-			if any(char.isdigit() for char in job) == False: # Check if picked phrase does not contain a number, otherwise is could be a street
-				if any(substring in job for substring in address_list): # Check if the phrase contains a clue that it still is a street, if not add to job list
-					if job[0].isupper() == False: # Check if the first letter of the phrase is not uppercase, otherwise it is a street
-						job_list.append(job) # Add the job phrase to the list of jobs
-				else:
-					job_list.append(job)
+	#job_list = [] # The list of jobs mentioned on the page
 
-job_dict = dict(Counter(job_list)) # Count the unique jobs a make a dictionary where key is job and value is count
-print(job_dict)
+	for sentence in text_strip:
+		splits = sentence.split(",") # Split the sentence on commas
+		if sentence[0][0] == "-": # Determine if the first character of the sentence is "-"
+			job_index = 1 # If the sentence starts with "-", the job is often the second phrase of the sentence
+		else:
+			job_index = 2 # Job is often the third phrase of the sentence, due to full names of the people
+		
+		if len(splits) > job_index: # Prevent index out of range
+			job = splits[job_index] # Extract the job phrase
+			job = job.strip(" ") # Remove the spaces on both sides of the phrase
+			if len(job) > 3: # Sometimes "van" or "der" or at the index of the ob, so we exclude those
+				if any(char.isdigit() for char in job) == False: # Check if picked phrase does not contain a number, otherwise is could be a street
+					if any(substring in job for substring in address_list): # Check if the phrase contains a clue that it still is a street, if not add to job list
+						if job[0].isupper() == False: # Check if the first letter of the phrase is not uppercase, otherwise it is a street
+							#job_list.append(job) # Add the job phrase to the list of jobs
+							big_dict[job] = big_dict.get(job, 0) + 1 # Add job to dict or increase count by 1
+					else:
+						#job_list.append(job)
+						big_dict[job] = big_dict.get(job, 0) + 1 # Add job to dict or increase count by 1
+
+	#job_dict = dict(Counter(job_list)) # Count the unique jobs a make a dictionary where key is job and value is count
+	#print(job_dict)
+print(big_dict)
